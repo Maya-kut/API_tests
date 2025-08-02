2156287db64e3dd077590b62dd7cb6a840ec6aa7
maja <maya-kut@mail.ru>
2025-08-02 15:29:51 +0300

    add api tests

--- src/test/java/tests/ReqresInTests.java
+++ src/test/java/tests/ReqresInTests.java
@@ -10,8 +10,8 @@ public class ReqresInTests extends TestBase {
     @Test
     void successfulCreateUserTest() {
         String newUser = "{\n" +
-                "    \"name\": \"morpheus\",\n" +
-                "    \"job\": \"leader\"\n" +
+                "    \"name\": \"Hercules\",\n" +
+                "    \"job\": \"God\"\n" +
                 "}";
 
         given()
@@ -25,8 +25,83 @@ public class ReqresInTests extends TestBase {
                 .log().status()
                 .log().body()
                 .statusCode(201)
-                .body("name", is("morpheus"))
-                .body("job", is("leader"));
+                .body("name", is("Hercules"))
+                .body("job", is("God"));
 
     }
+
+    @Test
+    void successfulUpdateUserTest() {
+        Integer userId = 2;
+        String newUserInfo = "{\n" +
+                "    \"name\": \"Hercules\",\n" +
+                "    \"job\": \"God\"\n" +
+                "}";
+
+        given()
+                .header("x-api-key", apiKey)
+                .body(newUserInfo)
+                .contentType(JSON)
+                .log().uri()
+                .when()
+                .post(("users" + "/" + userId))
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("Hercules"))
+                .body("job", is("God"));
+
     }
+
+    @Test
+    void unSuccessfulRegisterUserTest() {
+        String logoPass = "{\"email\": \"sydney@fife\"}";
+
+        given()
+                .header("x-api-key", apiKey)
+                .body(logoPass)
+                .contentType(JSON)
+                .log().uri()
+                .when()
+                .post("register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+
+    }
+
+    @Test
+    void getSingleUserTest() {
+        Integer userId = 2;
+        String userEmail = "janet.weaver@reqres.in";
+        given()
+                .header("x-api-key", apiKey)
+                .log().uri()
+                .when()
+                .get("users" + "/" + userId)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", is(userId))
+                .body("data.email", is(userEmail));
+
+    }
+
+    @Test
+    void deleteUserTest() {
+        Integer userId = 2;
+        given()
+                .header("x-api-key", apiKey)
+                .log().uri()
+                .when()
+                .delete("users" + "/" + userId)
+                .then()
+                .log().status()
+                .statusCode(204);
+    }
+    }
+
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,14 +5,13 @@ import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
 
-    public String registerEndpoint = "/register";
+
     public String apiKey = "reqres-free-v1";
-    public String usersEndpoint = "/users/";
 
     @BeforeAll
     static void beforeAll() {
 
         RestAssured.baseURI = "https://reqres.in";
-        RestAssured.basePath = "/api";
+        RestAssured.basePath = "/api/";
     }
 }
